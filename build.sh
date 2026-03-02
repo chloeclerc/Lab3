f36d1e99ddabc6802604b59cf213201a4e7c1ac7
chloe-clerc <c_clerc@apple.com>
2026-03-01 21:08:14 -0500

    Fix PostgreSQL driver: psycopg2-binary, Python 3.11, build verification

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
+pip install --upgrade pip
 pip install -r requirements.txt
+python -c "import psycopg2; print('psycopg2 OK')"
 python manage.py collectstatic --no-input
 python manage.py migrate
